175488dadc8720c7fde256aa9c46e87ad56e56cd
Semenov Roman <3908441@gmail.com>
2020-10-30 20:55:11 +0300

    add repo find

--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 from os import makedirs, getenv
 
+
 def repo_create(path: Path) -> Path:
+    DIRNAME = getenv("GIT_DIR") or ".git"
+
     if path.is_file():
         raise Exception(f"{path} is not a directory")
 
-    dirname = getenv("GIT_DIR") or ".git"
-    gitdir = path / dirname
+    gitdir = path / DIRNAME
     
     dirs = [
         gitdir / "refs" / "heads",
@@ -27,3 +29,18 @@ def repo_create(path: Path) -> Path:
         file.write("Unnamed pyvcs repository.\n")
 
     return gitdir
+
+def repo_find(path: Path = Path(".")) -> Path:
+    DIRNAME = getenv("GIT_DIR") or ".git"
+    
+    path = path.absolute()
+    while True:
+        if DIRNAME in (x.name for x in path.iterdir()):
+            return path / DIRNAME
+        
+        if path == path.parent:
+            break
+
+        path = path.parent
+
+    raise Exception("Not a git repository")
